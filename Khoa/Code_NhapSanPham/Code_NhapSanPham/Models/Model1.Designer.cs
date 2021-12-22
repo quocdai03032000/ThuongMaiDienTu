0d6b42e8249441f2502a2667f838333fbd122706
quocdai03032000 <quocdai03032000@gmail.com>
2021-12-22 23:21:57 +0700

    update code AddItem

--- Khoa/Code_NhapSanPham/Code_NhapSanPham/Models/Model1.Designer.cs
+++ Khoa/Code_NhapSanPham/Code_NhapSanPham/Models/Model1.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\laanh\OneDrive\Máy tính\Code_NhapSanPham\Code_NhapSanPham\Models\Model1.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Tran Quoc Dai\Desktop\Đồ án\Thương Mại Điện Tử\ThuongMaiDienTu\Khoa\Code_NhapSanPham\Code_NhapSanPham\Models\Model1.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
---
+++ Đại/FontEnd_admin/ThuongMaiDienTu_v1.0.1/ThuongMaiDienTu_v1.0.1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ThuongMaiDienTu_v1._0._1.MvcApplication" Language="C#" %>
