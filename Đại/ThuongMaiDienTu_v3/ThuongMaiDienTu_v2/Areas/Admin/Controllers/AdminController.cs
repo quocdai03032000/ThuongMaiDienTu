2012e9db102bbf9f670631b325811e52a9267795
DESKTOP-KCQO5DU\laanh <laanhkhoa8002gmail.com>
2022-01-06 15:24:52 +0700

    code v3 ThemSanPham

--- Đại/ThuongMaiDienTu_v3/ThuongMaiDienTu_v2/Areas/Admin/Controllers/AdminController.cs
+++ Đại/ThuongMaiDienTu_v3/ThuongMaiDienTu_v2/Areas/Admin/Controllers/AdminController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -36,7 +37,7 @@ namespace ThuongMaiDienTu_v2.Areas.Admin.Controllers
         //Product Management
         public ActionResult ProductManagement()
         {
-            return View();
+            return View(database.SanPhams);
         }
 
         //Order
@@ -66,7 +67,54 @@ namespace ThuongMaiDienTu_v2.Areas.Admin.Controllers
             TempData["ErrorLogin"] = "Sai tên tài khoản hoặc mật khẩu !";
             return View("Login");
         }
-        
+        // Thêm Sản phẩm
+
+        [HttpPost]
+        public ActionResult ThemSanPham(SanPham sp, string name, string desc, int price, int tyleID, string imageUploader, string imageUploader2, string imageUploader3, string imageUploader4, string imageUploader5)
+        {
+               if (sp.imageUploader != null)
+            {
+                var check = database.SanPhams.Where(a => a.SanPham_Name == name).SingleOrDefault();
+                if (check == null)
+                {
+                    string fileName = Path.GetFileNameWithoutExtension(sp.imageUploader.FileName);
+                    string extension = Path.GetExtension(sp.imageUploader.FileName);
+                    fileName = fileName + extension;
+                    string fileName2 = Path.GetFileNameWithoutExtension(sp.imageUploader2.FileName);
+                    string extension2 = Path.GetExtension(sp.imageUploader2.FileName);
+                    fileName2 = fileName2 + extension2;
+                    string fileName3 = Path.GetFileNameWithoutExtension(sp.imageUploader3.FileName);
+                    string extension3 = Path.GetExtension(sp.imageUploader3.FileName);
+                    fileName3 = fileName3 + extension3;
+                    string fileName4 = Path.GetFileNameWithoutExtension(sp.imageUploader4.FileName);
+                    string extension4 = Path.GetExtension(sp.imageUploader4.FileName);
+                    fileName4 = fileName4 + extension4;
+                    string fileName5 = Path.GetFileNameWithoutExtension(sp.imageUploader5.FileName);
+                    string extension5 = Path.GetExtension(sp.imageUploader5.FileName);
+                    fileName5 = fileName5 + extension5;
+
+                    sp.Main_img = "~/Content/Image_SanPham/" + fileName;
+                    sp.Img1 = "~/Content/Image_SanPham/" + fileName2;
+                    sp.Img2 = "~/Content/Image_SanPham/" + fileName3;
+                    sp.Img3 = "~/Content/Image_SanPham/" + fileName4;
+                    sp.Img4 = "~/Content/Image_SanPham/" + fileName5;
+                    sp.imageUploader.SaveAs(Path.Combine(Server.MapPath("~/Content/Image_SanPham/"), fileName));
+
+                    sp.imageUploader2.SaveAs(Path.Combine(Server.MapPath("~/Content/Image_SanPham/"), fileName2));
+                    sp.imageUploader3.SaveAs(Path.Combine(Server.MapPath("~/Content/Image_SanPham/"), fileName3));
+                    sp.imageUploader4.SaveAs(Path.Combine(Server.MapPath("~/Content/Image_SanPham/"), fileName4));
+                    sp.imageUploader5.SaveAs(Path.Combine(Server.MapPath("~/Content/Image_SanPham/"), fileName5));
+                    sp.SanPham_Name = name;
+                    sp.SanPham_description = desc;
+                    sp.SanPham_Price = price;
+                    sp.Type_SanPham_id = tyleID;
+                    database.SanPhams.Add(sp);
+                    database.SaveChanges();
+                }
+            }
+             
+            return RedirectToAction("ProductManagement", "Admin");
+        }
 
 
     }
